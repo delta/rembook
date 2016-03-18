57f24c4a00cfca1a374240291749c26d34c04366
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-18 16:00:25 +0530

    Add Funciton to Create Rem

--- models/Rems.js
+++ models/Rems.js
@@ -13,30 +13,39 @@ var RemsSchema = new mongoose.Schema({
 
 var Rem = mongoose.model('Rem', RemsSchema);
 
-module.exports.createRem = function (remData, next) {
+var createRem = function (remData, next) {
   var rem = new Rem(remData);
-  rem.save(function (err, doc) {
-    if(!err){
-      var remFrom = doc.from;
-      var remid = doc._id;
-      var remTo = doc.to;
-      if (remTo === remFrom){
-        var update = {
-          myrem : remid,
-        };
-        Users.updateUser(remTo, update, next);
-      }else{
-        Users.getUserByRollno(remTo, function (err, doc) {
-          if(!err){
-            var rems = doc.rems;
-            rems.push(remid);
-            var update = {
-              rems : rems,
-            };
-            Users.updateUser(remTo, update, next);
-          }
-        });
-      }
+  Users.getUserByRollno(rem.from,function (err, doc) {
+    if (err) return next(err);
+    if (doc){
+      // console.log(rem.from,":",JSON.stringify(doc));
+      Users.getUserByRollno(rem.to, function (err, doc) {
+        if (err)return next(err);
+        if (doc){
+          // console.log(rem.to,":",JSON.stringify(doc));
+          var rems = doc.rems;
+          rem.save(function (err, doc) {
+            if (err) return next(err);
+            if (doc){
+              var remFrom = doc.from;
+              var remid = doc._id;
+              var remTo = doc.to;
+              if (remTo === remFrom){
+                Users.updateUser(remTo, {myrem:remid}, function(err, updatedUser){
+                  next(err,rem,updatedUser);
+                });
+              }else {
+                rems.push(remid);
+                Users.updateUser(remTo, {rems:rems}, function(err, updatedUser){
+                  next(err,rem,updatedUser);
+                });
+              }
+            }
+          });
+        }
+      });
     }
   });
 };
+
+module.exports.createRem = createRem;
