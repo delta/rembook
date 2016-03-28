ed45ca8732a266178d18cbd3d3539a1b45c94112
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-28 09:14:50 +0530

    Add users schema

---
+++ models/Users.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name:               String,
+  rollNumber:         String,
+  email:              String,
+  dob:                Date,
+  department:         String,
+  photoName:          String,
+  hardCopyRequested:  Boolean,
+  lastLogin:         { type: Date, default: Date.now },
+});
+
+var User = mongoose.model('User', userSchema);
+
+//Add Functions Below
