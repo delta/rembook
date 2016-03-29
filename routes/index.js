a12a565b00887aea14748e5caf5b83250559cd66
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-29 20:53:47 +0530

    Add department codes to init object for main page

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,10 @@ var users = require('../controllers/users');
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Rembook' });
 });
-
 router.post('/login',login.processLogin);
-
 router.get('/', login.initalPage);
-
 router.get('/profile/:rollNumber',users.getUserByRollNumber);
-
 router.post('/profile/',users.updateProfile);
+
 
 module.exports = router;
