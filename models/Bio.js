7cc1095984dc84f12ee6189f15d7f4026721b9bd
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-29 18:54:19 +0530

    Add schema for bio

---
+++ models/Bio.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var bioSchema = new Schema({
+  user:       String,
+  responses:  [{
+                questionId:   Schema.Types.ObjectId,
+                response:     String,
+              }]
+});
+
+var Bio = mongoose.model('Bio', bioSchema);
+
+var getBioOf = function (rollNumber) {
+  return Bio.findOne({user:rollNumber});
+};
+
+var editBioOf = function (rollNumber, data, callback) {
+  Bio.findOne({user:rollNumber}).then(function(doc) {
+    // if (doc === null){
+    //   doc = new Bio();
+    // }
+    doc.user = rollNumber;
+    doc.responses = data;
+    doc.save().then(function(){
+      callback(null,doc);
+    }).catch(function (err) {
+      console.log(err);
+      callback(err);
+    });
+  });
+};
+
+module.exports.Bio = Bio;
+module.exports.getBioOf = getBioOf;
+module.exports.editBioOf = editBioOf;
