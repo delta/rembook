de07d4f0ea896d438c64fe93d5bc9cdeed111da9
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-29 20:29:58 +0530

    Add more functions to authenticate, move login route function to authenticate.js

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var webmail = require('../controllers/authenticate');
+var login = require('../controllers/authenticate');
 var users = require('../controllers/users');
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Rembook' });
 });
 
-router.post('/login',function (req, res, next) {
-  req.session.username=username;
-  var username = req.body.username;
-  var password = req.body.password;
-  var callback = function (fail, success) {
-    if (fail){
-      res.set("X-Rembook-Login","Fail");
-      res.redirect("/login");
-    }else{
-      res.set("X-Rembook-Login","Authenticated");
-      req.session.username = username;
-      res.redirect("/");
-    }
-  };
-  webmail.authenticate(username, password, callback);
-});
+router.post('/login',login.processLogin);
 
-router.get('/',function (req, res, next) {
-  res.render('index', { title: 'Rembook' });
-});
+router.get('/', login.initalPage);
 
 router.get('/profile/:rollNumber',function (req, res, next) {
   var rollNumber = req.params.rollNumber;
