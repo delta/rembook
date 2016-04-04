b83c6e612a780c79f0345962ab8443d570c6059c
rizwanhkm <rizwan.hkm@gmail.com>
2016-04-04 15:56:42 +0530

    Fix Typo

--- misc/addQuestions.js
+++ misc/addQuestions.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb://localhost/rembook');
 var Qs = [
   {title:"nicknames", description:"Nicknames of", bio_description:"My Nicknames"},
   {title:"about", description:"A Few words about", bio_description:"About Me" },
-  {title:"features",description:"The first thing that strikes you about", bio_description:"My Strinking Features"},
+  {title:"features",description:"The first thing that strikes you about", bio_description:"My Striking Features"},
   {title:"words",description:"Frequently used Words by", bio_description:"Frequently Uttered Words" }
 ];
 
