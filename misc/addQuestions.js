da701777cb30fb676da9efb699253714c243ca73
rizwanhkm <rizwan.hkm@gmail.com>
2016-04-04 13:22:22 +0530

    Add script to add questions to database

---
+++ misc/addQuestions.js
@@ -0,0 +1,12 @@
+var Questions = require('../models/Questions');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rembook');
+
+var Qs = [
+  {title:"nicknames", description:"Nicknames of", bio_description:"My Nicknames"},
+  {title:"about", description:"A Few words about", bio_description:"About Me" },
+  {title:"features",description:"The first thing that strikes you about", bio_description:"My Strinking Features"},
+  {title:"words",description:"Frequently used Words by", bio_description:"Frequently Uttered Words" }
+];
+
+Questions.saveQuestion(Qs);
