d1a9a12e6c0e8cf6afcec56d2b0dc0758315dfd2
thakkarparth007 <thakkarparth007@gmail.com>
2016-04-05 01:29:09 +0530

    Fix routing bugs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,10 +7,13 @@ var RemBook = require('./models/RemBook');
 var ProfilePageComponent = require('./components/ProfilePageComponent');
 var RemsComponent = require('./components/RemsComponent');
 var RemBookRouter = null;
+var $bookBlock = null;
+var bookBlock = null;
+var keyboardEventsHandlerRegistered = false;
 
 function goLeft() {
 	if(RemBook.currentRemPage >= 2) {
-		safelyTurnPageTo(RemBook.currentRemPage - 1);
+		safelyTurnPageTo(RemBook.currentRemBookOf.attributes.rollNumber, RemBook.currentRemPage - 1);
 	}
 	else {
 		$('.rem-bookblock').bookblock( 'prev' );
@@ -19,93 +22,80 @@ function goLeft() {
 
 function goRight() {
 	if(RemBook.currentRemPage <= RemBook.currentRemBookOf.Rems.models.length) {
-		safelyTurnPageTo(RemBook.currentRemPage + 1);
+		safelyTurnPageTo(RemBook.currentRemBookOf.attributes.rollNumber, RemBook.currentRemPage + 1);
 	}
 	else {
 		$('.rem-bookblock').bookblock( 'next' );
 	}
 }
 
-function safelyTurnPageTo(page) {
-	// a very dirty hack below.
-	// Changed the bookblock plugin to
-	// throw an error saying "RemBook Hack: BookBlock currently animating"
-	// whenever it is currently animating and we command it 
-	// to jump. This is required because BookBlock ignores any commands
-	// given to it while it is animating, and we need to know 
-	// when it ignores that, so that we don't update the 
-	// current window location.
-	try {
-		RemBook.loadRemPage(RemBook.currentRemBookOf.attributes.rollNumber, page);
-	}
-	catch(e) {
-		if(/hack/i.test(e.message))
-			return;
-	}
+function safelyTurnPageTo(rollNumber, page) {
+	if (!bookBlock || bookBlock.bookblock('isActive'))
+		return setTimeout(function() {
+			safelyTurnPageTo(rollNumber, page)
+		}, 500);
+
+	RemBook.loadRemPage(rollNumber, page);
 	if(page == 1) {
-		RemBookRouter.navigate(RemBook.currentRemBookOf.attributes.rollNumber + "/profile/");
+		RemBookRouter.navigate(rollNumber + "/profile/");
 	}
 	else {
 		RemBookRouter.navigate(RemBook.currentRemBookOf.attributes.rollNumber + "/rems/" + (page-1));
 	}
 }
 
+function bookBlockKeyHandler(e) {
+	if(e.target != document.body) return true;
+	var keyCode = e.keyCode || e.which;
+	var arrow = {
+		left : 37,
+		up : 38,
+		right : 39,
+		down : 40
+	};
+
+	switch (keyCode) {
+		case arrow.left:
+		goLeft();
+		break;
+		case arrow.right:
+		goRight();
+		break;
+	}
+}
+
 function restartBookBlock() {
-	var Page = (function() {
-
-		var config = {
-			$bookBlock : $( '.rem-bookblock' )
-		},
-		init = function() {
-			config.$bookBlock.bookblock( {
-				speed : 1000,
-				shadowSides : 0.8,
-				shadowFlip : 0.5
-			} );
-			initEvents();
-		},
-		initEvents = function() {
-
-			var $slides = config.$bookBlock.children();
-
+	$bookBlock = $( '.rem-bookblock' );
+	var init = function() {
+		bookBlock = $bookBlock.bookblock({
+			speed : 1000,
+			shadowSides : 0.8,
+			shadowFlip : 0.5
+		});
+		initEvents();
+	};
+	var initEvents = function() {
+		var $slides = $bookBlock.children();
 			// add swipe events
-			$slides.on( {
-				'swipeleft' : function( event ) {
-					config.$bookBlock.bookblock( 'next' );
-					return false;
-				},
-				'swiperight' : function( event ) {
-					config.$bookBlock.bookblock( 'prev' );
-					return false;
-				}
-			} );
+		$slides.on( {
+			'swipeleft' : function( event ) {
+				$bookBlock.bookblock( 'next' );
+				return false;
+			},
+			'swiperight' : function( event ) {
+				$bookBlock.bookblock( 'prev' );
+				return false;
+			}
+		} );
 
-			// add keyboard events
-			$( document ).keydown( function(e) {
-				if(e.target != document.body) return true;
-				var keyCode = e.keyCode || e.which;
-
-				var arrow = {
-					left : 37,
-					up : 38,
-					right : 39,
-					down : 40
-				};
-
-				switch (keyCode) {
-					case arrow.left:
-					goLeft();
-					break;
-					case arrow.right:
-					goRight();
-					break;
-				}
-			});
-		};
-
-		return { init : init };
-	})();
-	Page.init();
+		// add keyboard events
+		if(!keyboardEventsHandlerRegistered) {
+			keyboardEventsHandlerRegistered = true;
+			$( document ).keydown( bookBlockKeyHandler );
+		}
+	};
+	
+	init();
 }
 
 function renderRems() {
@@ -159,13 +149,13 @@ var _RemBookRouter = Backbone.Router.extend({
 		this.navigate(RemBook.currentUser.attributes.rollNumber + "/profile/", { trigger: true });
 	},
 	"profile": function profile(rollNumber) {
-		safelyTurnPageTo(1);
+		safelyTurnPageTo(rollNumber, 1);
 	},
 	"rems": function rems(rollNumber, page) {
-		page = parseInt(page || 2);
-		page = page > 1 ? page : 2;
+		page = parseInt(page || 1);
+		page = page > 1 ? (page+1) : 2;
 
-		safelyTurnPageTo(page);
+		safelyTurnPageTo(rollNumber, page);
 	},
 	"writeRem": function writeRem(rollNumber) {
 		alert("Screw you");
@@ -174,5 +164,5 @@ var _RemBookRouter = Backbone.Router.extend({
 
 RemBookRouter = new _RemBookRouter();
 
+restartBookBlock();
 Backbone.history.start();
-//onChangeBook();
