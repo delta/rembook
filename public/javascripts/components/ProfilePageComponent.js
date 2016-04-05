0b8f8d99f3d86b77c30f057fc3bcc302affd2d3f
thakkarparth007 <thakkarparth007@gmail.com>
2016-04-05 11:24:33 +0530

    Add basic editing code

--- public/javascripts/components/ProfilePageComponent.js
+++ public/javascripts/components/ProfilePageComponent.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 
 var ProfilePageComponent = Vue.extend({
-    template: require('../../templates/ProfilePageComponent.tmpl')
+    template: require('../../templates/ProfilePageComponent.tmpl'),
+    computed: {
+        _isEditable_: function() {
+            return this.profile.lastLogin === null;
+        }
+    }
 });
 
-Vue.component('profile-page', ProfilePageComponent);
+Vue.component('profilepage-component', ProfilePageComponent);
 
 module.exports = ProfilePageComponent;
