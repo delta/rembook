418b3d055e01d14fe05399b351a68f75624b489c
Parth Thakkar <thakkarparth007@gmail.com>
2017-03-26 22:21:38 +0530

    Let anyone login - use ldap search filter
    This commit does away with maintaining separate department codes
    which are not very well organized by the LDAP server and reduces
    pain significantly.

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -10,99 +10,6 @@ var imaphost = "10.0.0.173";
 var imapport = 143;
 var domain = 'octa.edu';
 
-
-var getDepartment = function (rollNumber) {
-  var departmentCode = rollNumber.slice(0,4);
-  var department= "";
-  switch (departmentCode) {
-    case '1011': // UG Arch
-    department = "ARCHI";
-    break;
-    case '2023': // PG Chemm Engg (Energy Engg)
-    department = "CEESAT";
-    break;
-    case '1021': // UG Chem Engg
-    case '2021': // PG M.Tech Chem Engg
-    department = "CHL";
-    break;
-    case '2024': // MSc. Chem Engg
-    case '2041': // MSc. Chem Engg
-    department = "CHEM";
-    break;
-    case '1031': // UG Civil
-    case '2031': // PG Transportation Engg
-    case '2032': // PG Structural Engg
-    case '2034': // PG Environmental Engg
-    case '2035': // PG Environmental Engg
-    department = "CIV";
-    break;
-    case '1061': // UG CSE
-    case '2061': // PG CSE
-    case '3061': // PG CSE
-    department = "CSE";
-    break;
-    case '1071': // UG EEE
-    case '2071': // PG Power Systems
-    case '2072': // PG Power Electronics
-    case '3071': // PG Power Electronics
-    department = "EEE";
-    break;
-    case '1081': // UG ECE
-    case '2081': // PG Communication Systems
-    case '2082': // PG VLSI
-    department = "ECE";
-    break;
-    case '1101': // UG ICE
-    case '2101': // PG PCI
-    department = "ICE";
-    break;
-    case '1111':
-    case '2112': // Industrial safety
-    case '2113': // Thermal Power Engg
-    department = "MECH";
-    break;
-    case '1121': // UG MME
-    case '2122': // PG Material Science
-    case '2123': // PG Industrial Metallurgy
-    case '2121': // PG Welding
-    department = "MME";
-    break;
-    case '1141': // UG PROD
-    case '2141': // Manufacturing Tech
-    case '2142': // Industrial Engg and Mgmt
-    department = "PROD";
-    break;
-    case '2162': // actually, Msc. CS. But ldap doesn't differentiate between the two
-    department = "MCA";
-    break;
-    case '2051': // real MCA
-    department = "MCA";
-    break;
-    case '2131': // NDT
-    case '2132': // MSc. Phy
-    department = "PHY";
-    break;
-    case '2151': // DOMS
-    department = "MBA";
-    break;
-  }
-  return department;
-};
-
-var generateDN = function (rollNumber) {
-  var year = ",OU=20"+ rollNumber.slice(4,6);
-  var department = getDepartment(rollNumber);
-  if (department === "ARCHI") {
-    DN = "CN="+rollNumber+year+",OU="+department+",DC=octa,DC=edu";
-  }else{
-    var pgOrUg = rollNumber[0] == "1" ? "UG" : "PG";
-    if (/^216/.test(rollNumber)) year = ",OU=OR & CA";
-    if (/^(2132|2041)/.test(rollNumber)) year = "";
-    DN = "CN="+rollNumber+year+",OU="+pgOrUg+",OU="+department+",DC=octa,DC=edu";
-  }
-  return DN;
-};
-
 var getUserInfo= function(username, callback){
   var client = ldap.createClient({
     url: ldapurl
@@ -112,16 +19,20 @@ var getUserInfo= function(username, callback){
   var cn = server+'@'+domain;
   client.bind(cn,password,function(err){ console.log(err); });
 
+  if(/^\d{9}/.test(username) == false) return callback(new Error("Invalid roll number"));
   var opts = {
     scope: 'sub',
+    filter: "(cn=" + username + ")",
   };
-  var DN = generateDN(username);
+  var DN = "dc=octa,dc=edu";
   client.search(DN, opts, function(err, res) {
     if (err){
       callback(err);
     }else{
       res.on('searchEntry', function(entry) {
-        callback(null,entry.object);
+        var ret = Object.assign({}, entry.object);
+        ret.department = entry.object.dn.match(/OU=([^,]+),DC=octa/)[1];
+        callback(null,ret);
         client.unbind(function (err) {});
       });
       // res.on('searchReference', function(referral) {
@@ -238,15 +149,15 @@ var processLogin = function (req, res, next) {
     }else{
       console.log("Creds match");
       var year = parseInt( "20"+ username.slice(4,6));
-      if (year <= batch){
+      //if (year <= batch){
         var data = {};
         data.rollNumber = username;
         data.name = success.displayName.trim();
         data.email = data.rollNumber + "@nitt.edu";
-        data.department = getDepartment(username);
+        data.department = success.department;//etDepartment(username);
         data.lastLogin = Date.now();
         Users.createProfile(username, data, function (err,doc){ });
-      }
+      //}
       res.set("X-Rembook-Login","Authenticated");
       req.session.name = success.displayName.trim();
       req.session.rollNumber = username;
