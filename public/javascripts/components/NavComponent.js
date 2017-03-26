7c9f9e52edbe4326a8bfc1ae2c5d6523445ce8c2
Parth Thakkar <thakkarparth007@gmail.com>
2017-03-26 21:55:57 +0530

    Update NavComponent - show Rems for graudates, but hide write-button

--- public/javascripts/components/NavComponent.js
+++ public/javascripts/components/NavComponent.js
@@ -1,12 +1,45 @@
 import Vue from 'vue'
 
-var isFinalYear = require("../models/RemBook.js").isFinalYear;
-
 var NavComponent = Vue.extend({
     template: require('../../templates/NavComponent.tmpl'),
     computed: {
     	finalYear: function() {
-            return isFinalYear(this.RemBook.currentRemBookOf.Profile.attributes.rollNumber);
+	    var rollNumber = this.RemBook.currentRemBookOf.Profile.attributes.rollNumber;
+            var fourYearCode = "1" + (new Date().getFullYear() - 2014); // for UGs
+            var threeYearCode = "1" + (new Date().getFullYear() - 2013); // for MCA
+            var twoYearCode = "1" + (new Date().getFullYear() - 2012); // for other PGs
+            var finalYearCode = fourYearCode;
+
+            if(rollNumber[0] != "1") {
+                // MCA is a 3 year course. Everything else is 2 years.
+                if(/^2051/.test(rollNumber)) { // MCA
+                  finalYearCode = threeYearCode;
+                } else {
+                  finalYearCode = twoYearCode;
+                }
+            }
+
+	    var yearCode = rollNumber.substr(4, 2);
+	    return yearCode == finalYearCode || rollNumber == "203214030";
+        },
+    	graduated: function() {
+	    var rollNumber = this.RemBook.currentRemBookOf.Profile.attributes.rollNumber;
+            var fourYearCode = "1" + (new Date().getFullYear() - 2014); // for UGs
+            var threeYearCode = "1" + (new Date().getFullYear() - 2013); // for MCA
+            var twoYearCode = "1" + (new Date().getFullYear() - 2012); // for other PGs
+            var finalYearCode = fourYearCode;
+
+            if(rollNumber[0] != "1") {
+                // MCA is a 3 year course. Everything else is 2 years.
+                if(/^2051/.test(rollNumber)) { // MCA
+                  finalYearCode = threeYearCode;
+                } else {
+                  finalYearCode = twoYearCode;
+                }
+            }
+
+	    var yearCode = rollNumber.substr(4, 2);
+	    return yearCode <= finalYearCode;
         },
     	isProfilePage: function() {
     		return this.RemBook.currentRemPage == 1;
